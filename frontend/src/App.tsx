dac3eba6accd0de32afa2d18b8fdab55ce97be08
Lapidis2 <hitlapidis2@gmail.com>
2026-01-21 19:27:15 +0200

    added simple form to show how frontend consume api and display message for order placement

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [productId, setProductId] = useState('');
+  const[quantity,setQuantity]=useState(1);
+  const [message,setMessage]=useState('');
+  const handlingOrder=async()=>{
+    try {
+      const response=await axios.post("http://localhost:5000/order",{
+        productId,
+        quantity
+      })
+      setMessage(response.data.message)
+    } catch (error:any) {
+      setMessage(`order failed:${error.response?.data?.message||error.message}`)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{padding:"3rem"}}>
+      <h1>Inventory order system</h1>
+      <input type="text" value={productId} placeholder='enter product id' onChange={(e)=>setProductId(e.target.value)} />
+      <input type="number" min="1" value={quantity} placeholder='enter quantity' onChange={(e)=>setQuantity(Number(e.target.value))} style={{marginRight:"1.2rem",width:"3.5rem"}} />
+       <button onClick={handlingOrder}>Place Order</button>
+       <p>{message}</p>
+     </div>
     </div>
   );
 }
