44d1a163a3fc71be353a3930e47c38a4f1fa005a
Lapidis2 <hitlapidis2@gmail.com>
2026-01-21 15:20:54 +0200

    updated server to use the order route

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Express = require("express");
+import orderRouter from "./routes/order.route";
 const app=Express();
 app.use(Express.json());
+app.use(orderRouter);
 const port=5000
 app.listen(port,()=>{
     console.log(`server is running at port ${port}`)
