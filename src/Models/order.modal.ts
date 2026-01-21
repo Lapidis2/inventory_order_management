1b2eb77be65c4b83601fef7d832c11494ba7b4d0
Lapidis2 <hitlapidis2@gmail.com>
2026-01-21 13:05:46 +0200

    added order modal

---
+++ src/Models/order.modal.ts
@@ -0,0 +1,6 @@
+export interface orderModal{
+    orderId:number,
+    productId:number,
+    quantity:number,
+    status:string,
+}
--- src/Models/product.modal.ts
+++ src/Models/product.modal.ts
@@ -1,4 +1,4 @@
-export interface Product{
+export interface ProductModal{
     productId:number,
     productName:string,
     stock:number
