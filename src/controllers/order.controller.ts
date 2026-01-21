4ad7be387f6c10c6458121ea0aefab346de128e4
Lapidis2 <hitlapidis2@gmail.com>
2026-01-21 15:16:44 +0200

    added controller for the order

---
+++ src/controllers/order.controller.ts
@@ -0,0 +1,15 @@
+import { Request,Response } from "express";
+import * as orderService from "../services/order.service";
+export const createOrder=async(req:Request,res:Response)=>{
+    try {
+        const {productId,quantity}=req.body
+        const response=await orderService.addOrder(
+            productId,
+            quantity
+        );
+        res.status(201).json(response);
+    } catch (error) {
+        res.status(400).json({error:(error as Error).message});
+        
+    }
+}
